b370279a1aec9e0b57d9e6d18be7b32defe56b0c
antonvit1 <antonbntu@gmail.com>
2023-05-07 12:34:05 +0300

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,40 @@
 const fs = require("fs");
-const pathToFile = (__filename, "files");
-const filesCopy = (__filename, "files-copy");
-fs.copyFile(pathToFile, filesCopy, (error) => {
-    if (error) {
-        throw error;
-    } else {
-        console.log("Succes");
+const path = require("path");
+const fsPromises = fs.promises;
+const copyFiles = fsPromises.copyFile;
+const pathToFolder = path.join(__dirname, "files");
+const pathfilesCopy = path.join(__dirname, "files-copy");
+const rimraf = require("rimraf");
+
+fs.access(pathfilesCopy, (error) => {
+  if (!error) {
+  fs.rm(pathfilesCopy, { recursive: true }, (erro) => {
+    if (erro) {
+        throw erro;
+      } else {
+        createFolderAndCopyFile();
+      }
+  })
+  } else {
+    createFolderAndCopyFile();
+  }
+});
+
+
+
+function createFolderAndCopyFile() {
+    fs.mkdir(pathfilesCopy, (err) => {
+    if (err) {
+      throw err;
     }
-})
+    console.log("directory is created");
+  });
+
+fsPromises.readdir(pathToFolder).then((filesInFolder) => {
+      filesInFolder.forEach((everyFile) => {
+    const filePath = path.join(__dirname, "files", everyFile);
+    copyFiles(filePath, path.join(__dirname, "files-copy", everyFile));
+    console.log(everyFile);
+  });
+});
+}
